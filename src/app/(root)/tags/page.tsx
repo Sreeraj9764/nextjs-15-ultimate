7e97861795ced50bd0930ccd44bc7160f573567e
Sreeraj <sreeraj.c.n@growingstars.com>
2025-11-20 18:03:08 +0530

    feat: add getTags action and Tags component for fetching and displaying tags

---
+++ src/app/(root)/tags/page.tsx
@@ -0,0 +1,16 @@
+import { getTags } from "@/lib/actions/tag.actions";
+import React from "react";
+
+const Tags = async () => {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 2,
+  });
+
+  const tags = data || {};
+
+  console.log(JSON.stringify(tags, null, 2));
+  return <div>Tags</div>;
+};
+
+export default Tags;
